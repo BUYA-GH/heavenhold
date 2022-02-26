0c11c8b47596cb894ad1d572bc9ea52b05c0277f
BUYA-GH <baka3737baka@gmail.com>
2022-02-26 18:53:46 +0900

    0226 login style complete

--- front/src/components/login/Login/LoginWindow.js
+++ front/src/components/login/Login/LoginWindow.js
@@ -10,9 +10,30 @@ const LoginWindow = () => {
         <div className={cx('Wrapper')}>
             <div className={cx('LoginComp')}>
                 <div className={cx('LoginForm')}>
-                    Login
+                    <p className={cx('Title')}>
+                        Login
+                    </p>
+                    <input name="email" className={cx('textInput')} type="text" placeholder="이메일"/>
+                    <input name="password" className={cx('textInput')} type="password" placeholder="비밀번호"/>
+                    <div className={cx('LoginMid')}>
+                        <label className={cx('autoLogin')}>
+                            {" "}
+                            <input type="checkbox" id="hint" /> 로그인 유지하기
+                        </label>
+                        <div className={cx('autoLogin')}>아이디/비밀번호 찾기</div>
+                    </div>
+                    <div className={cx('LoginMid2')}>회원가입</div>
+                    <button className={cx('LoginBtn')}>
+                        로그인
+                    </button>
                 </div>
-                <div className={cx('LoginInfo')}/>
+                <div className={cx('LoginInfo')}>
+                    <p className={cx('InfoTitle')}>Welcome!</p>
+                    <div className={cx('InfoText')}>
+                        <p>서비스 사용을 위해서는 로그인이 필수입니다.</p>
+                        <p>일 안해!</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
